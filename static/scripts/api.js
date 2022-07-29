24e22c11f36899630ee95739985b928bc0ae7901
migdracios <migdracios@gmail.com>
2022-07-29 16:00:17 +0900

    Fix deploy URL to HTTPS

--- static/scripts/api.js
+++ static/scripts/api.js
@@ -7,7 +7,7 @@
 
 
 const backend_base_url = "http://127.0.0.1:8000"
-const deploy_base_url = "http://3.34.167.27"
+const deploy_base_url = "https://3.34.167.27"
 // const frontend_base_url = "http://127.0.0.1:5500"
 const frontend_base_url = "https://gomunity.shop"
 
