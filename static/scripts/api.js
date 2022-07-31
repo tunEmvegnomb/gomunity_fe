1cee87abaefcb03a8d107a8c1ef885a4b29b44e7
taein Kim <kti0940@gmail.com>
2022-07-31 22:14:06 +0900

    ADD-게시글,답변글 이미지 없이 저장 기능 완료

--- static/scripts/api.js
+++ static/scripts/api.js
@@ -159,7 +159,6 @@ async function getQuestions(){
 
 // 질문글 작성
 async function createQuestion() {
-
     const category = document.getElementsByClassName("article_category")[0];
     const category_value = category.options[category.selectedIndex].textContent;
     
@@ -168,13 +167,16 @@ async function createQuestion() {
     const content = document.getElementById("article_content").value;
     const image = document.getElementById("article_image").files[0];
 
+
     const formdata = new FormData();
     formdata.enctype = "multipart/form-data"
 
     formdata.append('title', title);
     formdata.append('hashtag', hashtag);
     formdata.append('content', content);
-    formdata.append('image', image);
+    if (image != undefined){
+        formdata.append('image', image);
+    }
 
     if (category_value === "질의응답"){
         const response = await fetch(`${backend_base_url}/qna/`,{
@@ -219,16 +221,16 @@ async function postComment() {
     const question_id = localStorage.getItem("question_id");
     const comment = document.getElementById("create_comment").value;
     let comment_img = document.getElementById("comment_img").files[0];
-    console.log("나와 이놈아");
-    console.log(comment_img);
-    if (comment_img == undefined){
-        comment_img = null
-    }
+
     const formdata = new FormData();
     formdata.enctype = "multipart/form-data"
-
+    
+    
     formdata.append('content', comment);
-    formdata.append('image', comment_img);
+
+    if (comment_img != undefined){
+        formdata.append('image', comment_img);
+    }
 
     const response = await fetch(`${backend_base_url}/qna/${question_id}/answer/`,{
         headers:{
@@ -239,8 +241,8 @@ async function postComment() {
         method: 'POST',
         body: formdata
     })
-    // const response_json = await response.json()
-    // console.log(response_json)
+
+    const response_json = await response.json()
     if (response.status == 200) {
         alert(response_json.message);
     }
